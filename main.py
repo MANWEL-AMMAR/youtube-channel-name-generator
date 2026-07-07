74dbf239860fc2d45ecc2d94d810c3bca71b6a02
MANWEL-AMMAR <manwelammar111@gmail.com>
2026-07-08 02:47:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("welcome to the youtube channel name genrator")
+user_name = input("what is your nickname\n")
+about = input("what is your channel about?\n")
+print("you could name your channel "+ (about + (" with "+ (user_name))) )
